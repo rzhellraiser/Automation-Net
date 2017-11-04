787488b412d7fd8f85ac32805af2d3c50478cf54
Rz <rz.dev.88@gmail.com>
2017-11-04 17:48:37 -0500

    Adding Get and Set methods that perform the steps for testing

---
+++ BasicProject/PropertiesCollection.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProject
+{
+
+    enum PropertyType
+    {
+        Id,
+        Name,
+        LinkText,
+        CssName,
+        ClassName
+    }
+
+    class PropertiesCollection
+    {
+        //Auto-implemented property
+        public static IWebDriver driver { get; set; }
+    }
+}
---
+++ BasicProject/SeleniumSetMethods.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace BasicProject
+{
+    class SeleniumSetMethods
+    {
+        //Enter Text
+        public static void EnterText(string element, string value, PropertyType elementtype)
+        {
+            if(elementtype == PropertyType.Id)
+                PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementtype == PropertyType.Name)
+                PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+        //Click into a button, Checkbox, option etc
+        public static void Click(string element, PropertyType elementtype)
+        {
+            if (elementtype == PropertyType.Id)
+                PropertiesCollection.driver.FindElement(By.Id(element)).Click();
+            if (elementtype == PropertyType.Name)
+                PropertiesCollection.driver.FindElement(By.Name(element)).Click();
+        }
+
+        //Selecting a drop down control
+        public static void SelectDropDown(string element, string value, PropertyType elementtype)
+        {
+            if (elementtype == PropertyType.Id)
+                new SelectElement(PropertiesCollection.driver.FindElement(By.Id(element))).SelectByText(value);
+            if (elementtype == PropertyType.Name)
+                new SelectElement(PropertiesCollection.driver.FindElement(By.Name(element))).SelectByText(value); 
+        }
+    }
+}
