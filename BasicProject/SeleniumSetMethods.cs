3337e1d1cc78c29008f3cc1414f37796c28f3555
Rz <rz.dev.88@gmail.com>
2017-11-18 23:16:26 -0500

    Reduced code by adding custom library methods

--- BasicProject/SeleniumSetMethods.cs
+++ BasicProject/SeleniumSetMethods.cs
@@ -11,30 +11,33 @@ namespace BasicProject
     class SeleniumSetMethods
     {
         //Enter Text
-        public static void EnterText(string element, string value, PropertyType elementtype)
+        public static void EnterText(IWebElement element, string value)
         {
-            if(elementtype == PropertyType.Id)
-                PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
-            if (elementtype == PropertyType.Name)
-                PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
+            element.SendKeys(value);
+            //if(elementtype == PropertyType.Id)
+            //    PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
+            //if (elementtype == PropertyType.Name)
+            //    PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
         }
 
         //Click into a button, Checkbox, option etc
-        public static void Click(string element, PropertyType elementtype)
+        public static void Click(IWebElement element)
         {
-            if (elementtype == PropertyType.Id)
-                PropertiesCollection.driver.FindElement(By.Id(element)).Click();
-            if (elementtype == PropertyType.Name)
-                PropertiesCollection.driver.FindElement(By.Name(element)).Click();
+            element.Click();
+            //if (elementtype == PropertyType.Id)
+            //    PropertiesCollection.driver.FindElement(By.Id(element)).Click();
+            //if (elementtype == PropertyType.Name)
+            //    PropertiesCollection.driver.FindElement(By.Name(element)).Click();
         }
 
         //Selecting a drop down control
-        public static void SelectDropDown(string element, string value, PropertyType elementtype)
+        public static void SelectDropDown(IWebElement element, string value)
         {
-            if (elementtype == PropertyType.Id)
-                new SelectElement(PropertiesCollection.driver.FindElement(By.Id(element))).SelectByText(value);
-            if (elementtype == PropertyType.Name)
-                new SelectElement(PropertiesCollection.driver.FindElement(By.Name(element))).SelectByText(value); 
+            new SelectElement(element).SelectByText(value);
+            //if (elementtype == PropertyType.Id)
+            //    new SelectElement(PropertiesCollection.driver.FindElement(By.Id(element))).SelectByText(value);
+            //if (elementtype == PropertyType.Name)
+            //    new SelectElement(PropertiesCollection.driver.FindElement(By.Name(element))).SelectByText(value); 
         }
     }
 }
